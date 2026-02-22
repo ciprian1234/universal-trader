0f5406a6b9011029afa30fcbd11c8eabfc014c9f
Ciprian Andrei <andrei.stefanel.ciprian@gmail.com>
2026-02-22 08:12:24 +0200

    disabled dai temp

--- watcher/src/config/platforms/ethereum.ts
+++ watcher/src/config/platforms/ethereum.ts
@@ -29,7 +29,7 @@ const ethereum: ChainConfig = {
     { address: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599', symbol: 'WBTC' },
     { address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', symbol: 'USDC' },
     { address: '0xdAC17F958D2ee523a2206206994597C13D831ec7', symbol: 'USDT' },
-    { address: '0x6B175474E89094C44Da98b954EedeAC495271d0F', symbol: 'DAI' },
+    // { address: '0x6B175474E89094C44Da98b954EedeAC495271d0F', symbol: 'DAI' },
   ],
 
   dexConfigs: [
